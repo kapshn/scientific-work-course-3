737fe2cdeae46c80930fd551d99564afd3b111b5
Igor Pervushin <virus_220v@mail.ru>
2019-06-14 18:39:47 +0700

    minor

--- light.js
+++ light.js
@@ -2,6 +2,7 @@ const TPLSmartDevice = require('tplink-lightbulb');
 const light = new TPLSmartDevice('192.168.1.56');
 
 module.exports.change_state = function(state, time, brightness) {
+  //console.log(state, time, brightness);
   light.send({
     'smartlife.iot.smartbulb.lightingservice': {
       'transition_light_state': {
