e7826e52c14b70bb59628db404e1ea62e768dbdd
Igor Pervushin <virus_220v@mail.ru>
2019-06-19 15:52:40 +0700

    big update
    add reset func for presets
    add preset file preset.json (it's in ignore, but u can rename preset_save.json)
    update readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scientific-work-course-3
 DEVELOPMENT OF A SYSTEM TO CONTROL A SMART LIGHT BULB USING A VOICE ASSISTANT
+rename preset_save.json to preset.json
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const light = require('./light.js');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-const data = require('./res.json')
+const data = require('./res.json');
+const preset = require('./preset.json');
 
 const app = express()
 // GET method route
@@ -23,13 +24,13 @@ app.post('/', function (req, res) {
     light.change_state(0, 1500, 15);
   }
   if (req.body.queryResult.intent.displayName == "intent turn on light") {
-    light.change_state(1, 1500, 15);
+    light.change_state(1, 1500, parseInt(preset.on,10));
   }
   if (req.body.queryResult.intent.displayName == "intent nightmode") {
-    light.change_state(1, 1500, 1);
+    light.change_state(1, 1500, parseInt(preset.nightmode,10));
   }
   if (req.body.queryResult.intent.displayName == "intent readmode") {
-    light.change_state(1, 1500, 25);
+    light.change_state(1, 1500, parseInt(preset.readmode,10));
   }
   if (req.body.queryResult.intent.displayName == "intent up brightness") {
     var iB = light.infoBrightness();
@@ -50,6 +51,40 @@ app.post('/', function (req, res) {
   if (req.body.queryResult.intent.displayName == "intent set brightness") {
     //console.log(req.body.queryResult.parameters.number);
     light.change_state(1, 1500, parseInt(req.body.queryResult.parameters.number,10));
+  }
+  if (req.body.queryResult.intent.displayName == "intent turn on preset") {
+    light.change_state(1, 1500, parseInt(preset.preset1,10));
+  }
+  if (req.body.queryResult.intent.displayName == "intent change") {
+    if (req.body.queryResult.parameters.Preset == "Preset"){
+      preset.preset1=req.body.queryResult.parameters.number;
+      fs.writeFile('./preset.json', JSON.stringify(preset, null, 2), function (err) {
+        if (err) return console.log(err);
+        console.log(JSON.stringify(preset));
+      });
+    }
+    if (req.body.queryResult.parameters.action == "on"){
+      preset.on=req.body.queryResult.parameters.number;
+      fs.writeFile('./preset.json', JSON.stringify(preset, null, 2), function (err) {
+        if (err) return console.log(err);
+        console.log(JSON.stringify(preset));
+      });
+    }
+    if (req.body.queryResult.parameters.light == "read"){
+      preset.readmode=req.body.queryResult.parameters.number;
+      fs.writeFile('./preset.json', JSON.stringify(preset, null, 2), function (err) {
+        if (err) return console.log(err);
+        console.log(JSON.stringify(preset));
+      });
+    }
+    if (req.body.queryResult.parameters.light == "night"){
+      preset.nightmode=req.body.queryResult.parameters.number;
+      fs.writeFile('./preset.json', JSON.stringify(preset, null, 2), function (err) {
+        if (err) return console.log(err);
+        console.log(JSON.stringify(preset));
+      });
+    }
+
   }
   res.status(200).json(data);
 });
